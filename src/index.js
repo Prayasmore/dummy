99c582f82ba4b3b1fd5dc87bbf10cb0428840a1a
Prayasmore <prayasbm21@gmail.com>
2024-09-11 16:10:31 +0530

    Feature: Add Paragraph component

--- src/index.js
+++ src/index.js
@@ -4,5 +4,6 @@ import Image from './Image';
 import Anchor from './Anchor';
 import OL from './OL';
 import Iframe from './Iframe';
+import Paragraph from './Paragraph';
 
-export { Button, Input, Image, Anchor, OL, Iframe };
+export { Button, Input, Image, Anchor, OL, Iframe, Paragraph };
